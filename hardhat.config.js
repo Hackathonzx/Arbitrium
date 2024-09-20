e873bcef5a4a71cadb0c224f65038b89d69a5e01
Jumcee <futhmah456@gmail.com>
2024-09-20 01:12:59 +0100

    i

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { RPC_URL, PRIVATE_KEY } = process.env
+
 module.exports = {
   solidity: "0.8.1",
-};
+  networks: {
+    arbitrumSepolia: {
+      url: process.env.ARBITRIUM_URL,
+      chainId: 421614,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+} 
